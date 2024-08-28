9bd24a83320c3284876bd767faad9940a47aae11
dalynoll <dalynoll@gmail.com>
2024-08-28 13:03:44 -0400

    Update 2_normalizar_header.sh

--- DAY_3/2_normalizar_header.sh
+++ DAY_3/2_normalizar_header.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directorios base
-practico=/Users/dalynoll/Dropbox/PRESENTACIONES_CHARLAS_Y_SEMINARIOS/2024_curso_Genomica-Valdivia/Practico_dia3/practico
+practico=/ruta/directorio/dia3
 agat="$practico/cds/1_agat"
 normalized="$practico/cds/1_agat_normalized"
 
